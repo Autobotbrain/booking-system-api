173c9140e1c71d70bff4cfc346b74137cdf87fe0
Autobotbrain <98964236+Autobotbrain@users.noreply.github.com>
2022-03-16 19:31:13 +0530

    First commit

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv =require('dotenv');
-const routesUrls =require('./routes/parent.js');
+const routesUrls =require('./routes/Driver.js');
 const cors = require('cors');
 
 dotenv.config();
 
-mongoose.connect(process.env.DATABASE_ACCESS, () => {
-  return console.log("Database connected")
-});
+ mongoose.connect(process.env.DATABASE_ACCESS, () => {
+   return console.log("Database connected")
+ });
 
 app.use(express.json());
 app.use(cors());
-app.use('/parent',  routesUrls);
+app.use('/Driver',  routesUrls);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
@@ -24,5 +24,5 @@ console.log(process.env.DATABASE_ACCESS)
 
 
 
-var parentRouter = require('./routes/parent.js');
-app.use('/parent',parentRouter);
+var DriverRouter = require('./routes/Driver.js');
+app.use('/Driver',DriverRouter);
